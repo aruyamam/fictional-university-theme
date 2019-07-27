6257b42b98723445562698eb0b710291251120a8
aruyamam <aruyamam@gmail.com>
2019-07-27 12:46:52 +0900

    Create Your Own Raw JSON Data

--- inc/search-route.php
+++ inc/search-route.php
@@ -10,7 +10,59 @@ function universityRegisterSearch()
    ]);
 }
 
-function universitySearchResults()
+function universitySearchResults($data)
 {
-   return 'Congraturation, you created a route.';
+   $mainQuery = new WP_Query([
+      'post_type' => ['post', 'page', 'professor', 'program', 'campus', 'event'],
+      's' => sanitize_text_field($data['term'])
+   ]);
+
+   $results = [
+      'generalInfo' => [],
+      'professors' => [],
+      'programs' => [],
+      'events' => [],
+      'campuses' => []
+   ];
+
+   while ($mainQuery->have_posts()) {
+      $mainQuery->the_post();
+
+      if (get_post_type() === 'post' || get_post_type() === 'page') {
+         array_push($results['generalInfo'], [
+            'title' => get_the_title(),
+            'permalink' => get_the_permalink()
+         ]);
+      }
+
+      if (get_post_type() === 'professor') {
+         array_push($results['professors'], [
+            'title' => get_the_title(),
+            'permalink' => get_the_permalink()
+         ]);
+      }
+
+      if (get_post_type() === 'program') {
+         array_push($results['programs'], [
+            'title' => get_the_title(),
+            'permalink' => get_the_permalink()
+         ]);
+      }
+
+      if (get_post_type() === 'campus') {
+         array_push($results['campuses'], [
+            'title' => get_the_title(),
+            'permalink' => get_the_permalink()
+         ]);
+      }
+
+      if (get_post_type() === 'event') {
+         array_push($results['events'], [
+            'title' => get_the_title(),
+            'permalink' => get_the_permalink()
+         ]);
+      }
+   }
+
+   return $results;
 }
