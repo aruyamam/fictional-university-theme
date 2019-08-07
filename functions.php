e10b85fd715af66814c960dd85e276eb1af10fa7
aruyamam <aruyamam@gmail.com>
2019-08-07 18:23:34 +0900

    Open Registration

--- functions.php
+++ functions.php
@@ -98,3 +98,50 @@ function university_adjust_queries($query)
 }
 
 add_action('pre_get_posts', 'university_adjust_queries');
+
+// Redirect subscriber accounts out of admin and onto homepage
+add_action('admin_init', 'redirectSubsToFrontend');
+
+function redirectSubsToFrontend()
+{
+   $ourCurrentUser = wp_get_current_user();
+
+   if (count($ourCurrentUser->roles) === 1 && $ourCurrentUser->roles[0] === 'subscriber') {
+      wp_redirect('/');
+      exit;
+   }
+}
+
+add_action('wp_loaded', 'noSubsAdminBar');
+
+function noSubsAdminBar()
+{
+   $ourCurrentUser = wp_get_current_user();
+
+   if (count($ourCurrentUser->roles) === 1 && $ourCurrentUser->roles[0] === 'subscriber') {
+      show_admin_bar(false);
+   }
+}
+
+// Customize Login Screen
+add_filter('login_headerurl', 'ourHeaderUrl');
+
+function ourHeaderUrl()
+{
+   return esc_url(site_url('/'));
+}
+
+add_action('login_enqueue_scripts', 'ourLoginCSS');
+
+function ourLoginCSS()
+{
+   wp_enqueue_style('university_main_style', get_stylesheet_uri());
+   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+}
+
+add_filter('login_headertext', 'ourLoginText');
+
+function ourLoginText()
+{
+   return get_bloginfo('name');
+}
